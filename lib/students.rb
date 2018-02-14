d7c0e897e0a00b2518a55f668068a03da13c614d
Jen Poole <contact.vandepool@gmail.com>
2018-02-14 03:15:00 +0000

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -12,6 +12,7 @@ end
 
 # finds the average gpa
 def average_student_gpa
+  'SELECT AVG(gpa) FROM students;'
 end
 
 # finds the total amount of tardies for all students
@@ -20,4 +21,5 @@ end
 
 #  find the average gpa for 9th grade
 def average_gpa_for_9th_grade
+  'SELECT AVG(gpa) FROM students WHERE grade = 9;'
 end
